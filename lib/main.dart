71ac90083325a81373addbc42685f2cbc7be9c31
Rishabh <rishabhm.10.03@gmail.com>
2024-09-14 10:33:44 +0530

    added search screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:showcase/screens/auth_screen.dart';
-import 'package:showcase/screens/login_screen.dart';
-import 'package:showcase/screens/signup_screen.dart';
-import 'package:showcase/screens/splash_screen.dart';
+
+import 'screens/login_screen.dart';
+import 'screens/signup_screen.dart';
+import 'screens/auth_screen.dart';
+import 'screens/splash_screen.dart';
+import 'screens/search_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -15,7 +17,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'showcase',
-      home: SplashScreen(),
+      home: SearchScreen(),
       debugShowCheckedModeBanner: false,      
     );
   }
